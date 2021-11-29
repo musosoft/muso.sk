5fea0350983dec7a8b1cacda9fa9cf71a6922aca
Roman Kovac <musosoft@gmail.com>
2021-11-29 21:52:43 +0700

    Update

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,13 @@ module.exports = {
         name: 'pages',
       },
     },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
-      },
-    },
+    // {
+    //   resolve: 'gatsby-source-filesystem',
+    //   options: {
+    //     path: `${__dirname}/src/img`,
+    //     name: 'images',
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     {
       resolve: 'gatsby-transformer-remark',
